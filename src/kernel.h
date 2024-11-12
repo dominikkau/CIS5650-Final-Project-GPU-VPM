74da3501543fc3b9ee8d04381a4763c72591b59f
Nadine Adnane <nadnane@users.noreply.github.com>
2024-11-11 23:15:04 -0500

    Remove unnecessary code

--- src/kernel.h
+++ src/kernel.h
@@ -8,13 +8,6 @@
 #include <cmath>
 #include <vector>
 
-namespace Boids {
-    void initSimulation(int N);
-    void stepSimulationNaive(float dt);
-    void stepSimulationScatteredGrid(float dt);
-    void stepSimulationCoherentGrid(float dt);
-    void copyBoidsToVBO(float *vbodptr_positions, float *vbodptr_velocities);
+namespace GPUVPM {
 
-    void endSimulation();
-    void unitTest();
 }
