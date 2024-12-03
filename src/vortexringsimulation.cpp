bf15a582b71b3f94b30c4e076034714fa9b5c228
“Shreyas <shreyashsingh31@gmail.com>
2024-12-03 12:10:10 -0500

    change fun_length params for circular ring

--- src/vortexringsimulation.cpp
+++ src/vortexringsimulation.cpp
@@ -4,7 +4,6 @@
 #include <iostream>
 #include <vector>
 #include <math.h>
-#include <Eigen/Dense>
 #include <cmath>
 #include <functional>
 #include "vpmcore/subFilterScale.h"
@@ -29,27 +28,28 @@ void addVortexRing(ParticleField* pField, float circulation, float R, float AR, 
 int Nphi, int nc, float sigmas, int extra_nc, glm::vec3 ringPosition, 
 glm::mat3 ringOrientation){ // missing v_lvl from args
 
-    auto fun_S = [r](float phi) {
-        return r * phi; // assuming a circular ring and angle in radians
+    auto fun_S = [R](float phi) {
+        return R * phi; // assuming a circular ring and angle in radians
     };
 
     float Stot = fun_S(2 * M_PI);
 
+    // Non-dimensional arc length from 0 to a given value <=1
     auto fun_s = [fun_S, Stot](float phi) {
         return fun_S(phi) / Stot;
     };
 
     // Angle associated to a given non-dimensional arc length
-    auto fun_phi = [fun_s, a, b](float s) {
-        return fun_s * 2 * M_PI
+    auto fun_phi = [fun_s](float r) {
+        return r * 2 * M_PI;
         // if (std::abs(s) <= std::numeric_limits<float>::epsilon()) return 0.0f;
         // if (std::abs(s - 1) <= std::numeric_limits<float>::epsilon()) return 2 * M_PI;
         // return fzero([fun_s, s](float phi) { return fun_s(phi) - s; }, 0, 2 * M_PI - 1e-16); // CHECK
     }
 
-    auto fun_length = [fun_S](float r, float tht, float phi1, float phi2) {
-        float S1 = fun_S(phi1, r * std::cos(tht));
-        float S2 = fun_S(phi2, r * std::cos(tht));
+    auto fun_length = [fun_S, R](float r, float tht, float phi1, float phi2) {
+        float S1 = fun_S(phi1, R + r * std::cos(tht));
+        float S2 = fun_S(phi2, R + r * std::cos(tht));
         return S2 - S1;
     };
 
@@ -78,23 +78,26 @@ glm::mat3 ringOrientation){ // missing v_lvl from args
         float sc2 = ds * (N + 1);
         float sc = (sc1 + sc2) / 2;
 
-        float phi1 = fun_phi(sc1, a, b);
-        float phi2 = fun_phi(sc2, a, b);
-        float phic = fun_phi(sc, a, b);
+        float phi1 = fun_phi(sc1);
+        float phi2 = fun_phi(sc2);
+        float phic = fun_phi(sc);
 
-
-        glm::vec3 Xc(a * std::sin(phic), b * std::cos(phic), 0);
-        glm::vec3 T(a * std::cos(phic), -b * std::sin(phic), 0);
+        glm::vec3 Xc(a * std::sin(phic), b * std::cos(phic), 0); // Center of the cross section
+        glm::vec3 T(a * std::cos(phic), -b * std::sin(phic), 0); // Unitary tangent of the cross section
         T.normalize();
         T *= -1;
         glm::mat3 Naxis;
         Naxis.col(0) = T;
-        Naxis.col(1) = T.cross(Eigen::Vector3d(0, 0, 1));
-        Naxis.col(2) = Eigen::Vector3d(0, 0, 1);
+        Naxis.col(1) = glm::cross(glm::vec3(0, 0, 1), T);
+        Naxis.col(2) = glm::vec3(0, 0, 1);
 
-        auto dvol_wrap = [&fun_dvol, a, b, phi1, phi2](double r, double tht) {
+        auto dvol_wrap = [&fun_dvol, a, b, phi1, phi2](float r, float tht) {
             return fun_dvol(r, tht, a, b, phi1, phi2);
         };
+
+        float dvol_wrap = r * fun_length(r, tht, phi1, phi2);
+            
+        // volume - fun_vol (th1, th2) - Cubature(dvol_wrap) - fun_dvol - fun_length - 
 
         for (int n = 0; n <= nc + extra_nc; ++n) {
             if (n == 0) {
@@ -108,13 +111,13 @@ glm::mat3 ringOrientation){ // missing v_lvl from args
                 float vol = fun_vol(dvol_wrap, r1, tht1, r2, tht2);
 
                 // Position
-                array<float, 3> X = Xc;
+                glm::vec3 X = Xc;
 
                 // Vortex strength
-                array<float, 3> Gamma = {omega * vol * T[0], omega * vol * T[1], omega * vol * T[2]};
+                glm::vec3 Gamma = {omega * vol * T[0], omega * vol * T[1], omega * vol * T[2]};
 
                 // Filament length
-                float length = fun_length(0, 0, a, b, phi1, phi2);
+                float length = fun_length(0, R, phi1, phi2);
 
                 // Circulation
                 float crcltn = norm(Gamma) / length;
@@ -123,11 +126,36 @@ glm::mat3 ringOrientation){ // missing v_lvl from args
                 glm::vec3 Gamma_global = ringOrientation * Gamma;
                 pField->particles[idx].X = X_global;
                 pField->particles[idx].Gamma = Gamma_global;
+                pField->particles[idx].circulation = crcltn;
                 idx += 1;
-                } 
+                }
             else {
+                float rc = (1 + 12 * n * n) / (6 * n) * rl;  // Center radius
+                float r1 = (2 * n - 1) * rl;                // Lower radius
+                float r2 = (2 * n + 1) * rl;                // Upper radius
+                int ncells = 8 * n;                         // Number of cells
+                float deltatheta = 2 * M_PI / ncells;       // Angle of cells
 
-            }
+                // Discretize layer into cells around the circumference
+                for (int j = 0; j < ncells; ++j) {
+                    float tht1 = deltatheta * j;            // Left angle
+                    float tht2 = deltatheta * (j + 1);      // Right angle
+                    float thtc = (tht1 + tht2) / 2;         // Center angle
+
+                    float vol = fun_vol(dvol_wrap, r1, tht1, r2, tht2); // Volume
+
+                    glm::vec3 temp = glm::vec3(0, rc*std::cos(thtc), rc*std::sin(thtc));
+                    pField->particles[idx].X = Xc + Naxis * temp;
+
+                    if (n<=nc)
+                        pField->particles[idx].Gamma = omega * vol * T;
+                    else 
+                        pFielf->particles.Gamma = std::numeric_limits<float>epsilon() * T;
+                    
+                    float length = fun_length(rc, thtc, R, phi1, phi2);
+                    pField->particles[idx].circulation = Gamma.normalize)() / length;
+                }   
+            }   
         }
     }
 }
